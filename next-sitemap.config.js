1058ac19fd6a88ee9e17ff80beae7d0c4876afc2
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-06-21 01:38:45 +0200

    remove old sitemap

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://www.mzekealluspecialist.co.za',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-};
