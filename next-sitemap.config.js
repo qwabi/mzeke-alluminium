7f83d571b5b216ce732dea7502a2acd80dd31fb0
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-06-21 01:19:54 +0200

    add site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.mzekealluspecialist.co.za',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
