f8484924689e17ed7d89fca2f768c12e737273fe
Ayabonga Qwabi <ayabongaqwabi@gmail.com>
2025-06-21 02:16:18 +0200

    fix sitemap issue

---
+++ app/sitemap.ts
@@ -0,0 +1,11 @@
+export default async function sitemap() {
+  return [
+    {
+      url: 'https://www.mzekealluspecialist.co.za/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    // Add more URLs as needed
+  ];
+}
